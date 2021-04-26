866b4c29d5abb72408fd8e597d2cebcf14b66a34
Andriy Ivanyk <andriy@Air-Andriy.lan>
2021-04-26 02:31:56 +0100

    Updates

---
+++ precache-manifest.f8d03e65efbfcd7b7e7a911c89bdb6ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f2f0d20fbf2b0cf7e23432da02a1b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "08c7f62c4211adca9ff9",
+    "url": "./static/css/main.fd497770.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c7f62c4211adca9ff9",
+    "url": "./static/js/main.bfff2ac2.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
